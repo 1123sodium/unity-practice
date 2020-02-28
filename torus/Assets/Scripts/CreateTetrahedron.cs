1501690e15204fef4c09a7ca8529b1f68669095d
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2020-02-29 01:50:39 +0900

    add balls

--- torus/Assets/Scripts/CreateTetrahedron.cs
+++ torus/Assets/Scripts/CreateTetrahedron.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CreateTetrahedron : MonoBehaviour
 {
+    public GameObject ballPrefab;
+    public GameObject balls;
+
     [SerializeField]
     private Material _material;
     private Mesh _mesh;
@@ -18,10 +21,16 @@ public class CreateTetrahedron : MonoBehaviour
         var size = 2;
         var origin = new Vector3(0, 2, 0);
 
-        vertices.Add(origin + new Vector3(0, 0, 0));
-        vertices.Add(origin + new Vector3(size, 0, 0));
-        vertices.Add(origin + new Vector3(0, size, 0));
-        vertices.Add(origin + new Vector3(0, 0, size));
+        // vertices
+        var v0 = origin + new Vector3(0, 0, 0);
+        var v1 = origin + new Vector3(size, 0, 0);
+        var v2 = origin + new Vector3(0, size, 0);
+        var v3 = origin + new Vector3(0, 0, size);
+
+        vertices.Add(v0);
+        vertices.Add(v1);
+        vertices.Add(v2);
+        vertices.Add(v3);
 
         normals.Add(new Vector3(-1, -1, -1));
         normals.Add(new Vector3(1, 0, 0));
@@ -51,6 +60,24 @@ public class CreateTetrahedron : MonoBehaviour
         _mesh.normals = normals.ToArray();  // これがないと影がつかない
 
         _mesh.RecalculateBounds();
+
+        // 点の描画
+        var ballSize = 0.5f;
+        GameObject g0 = Instantiate(ballPrefab, balls.transform);
+        g0.transform.position = v0;
+        g0.transform.localScale = new Vector3(ballSize, ballSize, ballSize);
+
+        GameObject g1 = Instantiate(ballPrefab, balls.transform);
+        g1.transform.position = v1;
+        g1.transform.localScale = new Vector3(ballSize, ballSize, ballSize);
+
+        GameObject g2 = Instantiate(ballPrefab, balls.transform);
+        g2.transform.position = v2;
+        g2.transform.localScale = new Vector3(ballSize, ballSize, ballSize);
+
+        GameObject g3 = Instantiate(ballPrefab, balls.transform);
+        g3.transform.position = v3;
+        g3.transform.localScale = new Vector3(ballSize, ballSize, ballSize);
     }
     // Start is called before the first frame update
     void Start()
