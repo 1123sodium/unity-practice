2ca27729e7847ad26b01251ff65d9a041081e299
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2020-02-28 23:05:07 +0900

    add triangle and torus

---
+++ torus/Assets/Scripts/CreateTorus.cs
@@ -0,0 +1,73 @@
+﻿// using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateTorus : MonoBehaviour
+{
+    [SerializeField]
+    private Material _material;
+    private Mesh _mesh;
+ 
+    void Awake()
+    {
+        var r1 = 3.0f;
+        var r2 = 1.0f;
+        var n = 20;
+
+        _mesh = new Mesh();
+
+        var vertices = new List<Vector3>();
+        var triangles = new List<int>();
+        var normals = new List<Vector3>();
+
+        for (int i=0; i<=n; i++)
+        {
+            var phi = Mathf.PI * 2.0f * i / n;
+            var tr = Mathf.Cos(phi) * r2;
+            var y = Mathf.Sin(phi) * r2;
+
+            for (int j=0; j<=n; j++)
+            {
+                var theta = 2.0f * Mathf.PI * j / n;
+                var x = Mathf.Cos(theta) * (r1 + tr);
+                var z = Mathf.Sin(theta) * (r1 + tr);
+
+                vertices.Add(new Vector3(x, y, z));
+                normals.Add(new Vector3(tr * Mathf.Cos(theta), y, tr * Mathf.Sin(theta)));
+            }
+        }
+
+        for (int i=0; i<n; i++)
+        {
+            for (int j=0; j<n; j++)
+            {
+                var count = (n + 1) * j + i;
+                triangles.Add(count);
+                triangles.Add(count + n + 2);
+                triangles.Add(count + 1);
+
+                triangles.Add(count);
+                triangles.Add(count + n + 1);
+                triangles.Add(count + n + 2);
+            }
+        }
+
+        _mesh.vertices = vertices.ToArray();
+        _mesh.triangles = triangles.ToArray();
+        _mesh.normals = normals.ToArray();
+
+        _mesh.RecalculateBounds();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Graphics.DrawMesh(_mesh, Vector3.zero, Quaternion.identity, _material, 0);
+    }
+}
---
+++ torus/Assets/Scripts/CreateTriangle.cs
@@ -0,0 +1,46 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateTriangle : MonoBehaviour
+{
+    [SerializeField]
+    private Material _material;
+    private Mesh _mesh;
+    private Vector3[] _positions = new Vector3[]
+    {
+        new Vector3(0, 1, 0),
+        new Vector3(1, -1, 0),
+        new Vector3(-1, -1, 0)
+    };
+    private int[] _triangles = new int[] { 0, 1, 2 };
+    private Vector3[] _normals = new Vector3[]
+    {
+        new Vector3(0, 0, -1),
+        new Vector3(0, 0, -1),
+        new Vector3(0, 0, -1)
+    };
+
+    private void Awake()
+    {
+        _mesh = new Mesh();
+
+        _mesh.vertices = _positions;
+        _mesh.triangles = _triangles;
+        _mesh.normals = _normals;
+
+        _mesh.RecalculateBounds();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Graphics.DrawMesh(_mesh, Vector3.zero, Quaternion.identity, _material, 0);
+    }
+}
