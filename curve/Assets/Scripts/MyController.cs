4a2125e6dbb5e609133c206e4c4cc39982029ed7
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2020-03-24 00:58:00 +0900

    refactoring in ButtonMap

--- curve/Assets/Scripts/MyController.cs
+++ curve/Assets/Scripts/MyController.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 namespace MyUtil
 {
-    using ButtonMap = Dictionary<OVRInput.RawButton, KeyCode>;
+    // using ButtonMap = Dictionary<OVRInput.RawButton, KeyCode>;
+    public class ButtonMap
+    {
+        public KeyCode a;
+        public KeyCode b;
+        public KeyCode x;
+        public KeyCode y;
+
+        public ButtonMap(KeyCode a, KeyCode b, KeyCode x, KeyCode y)
+        {
+            this.a = a;
+            this.b = b;
+            this.x = x;
+            this.y = y;
+        }
+
+        public static ButtonMap defaultValue = new ButtonMap(
+            a: KeyCode.A, b: KeyCode.B, x: KeyCode.X, y: KeyCode.Y
+        );
+    }
 
     
     public class MyController
     {
         private GameObject rController;
         private ButtonMap buttonMap;
-
-        public static ButtonMap defaultButtonMap = new ButtonMap() {
-            { OVRInput.RawButton.A, KeyCode.A },
-            { OVRInput.RawButton.B, KeyCode.B },
-            { OVRInput.RawButton.X, KeyCode.X },
-            { OVRInput.RawButton.Y, KeyCode.Y }
-        };
 
         public MyController(ButtonMap buttonMap = null)
         {
@@ -27,7 +39,7 @@ namespace MyUtil
                 this.buttonMap = buttonMap;
             } else
             {
-                this.buttonMap = MyController.defaultButtonMap;
+                this.buttonMap = ButtonMap.defaultValue;
             }
         }
 
@@ -43,22 +55,22 @@ namespace MyUtil
 
         public bool GetAButton()
         {
-            return OVRInput.Get(OVRInput.RawButton.A) || GetKey(this.buttonMap[OVRInput.RawButton.A]);
+            return OVRInput.Get(OVRInput.RawButton.A) || GetKey(this.buttonMap.a);
         }
 
         public bool GetBButton()
         {
-            return OVRInput.Get(OVRInput.RawButton.B) || GetKey(this.buttonMap[OVRInput.RawButton.B]);
+            return OVRInput.Get(OVRInput.RawButton.B) || GetKey(this.buttonMap.b);
         }
 
         public bool GetXButton()
         {
-            return OVRInput.Get(OVRInput.RawButton.X) || GetKey(this.buttonMap[OVRInput.RawButton.X]);
+            return OVRInput.Get(OVRInput.RawButton.X) || GetKey(this.buttonMap.x);
         }
 
         public bool GetYButton()
         {
-            return OVRInput.Get(OVRInput.RawButton.Y) || GetKey(this.buttonMap[OVRInput.RawButton.Y]);
+            return OVRInput.Get(OVRInput.RawButton.Y) || GetKey(this.buttonMap.y);
         }
 
         public Vector2 GetLStick()
