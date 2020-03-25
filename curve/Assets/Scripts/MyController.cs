332bb5078b1d15f1d27a038e3032681cd163f7c8
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2020-03-26 00:41:49 +0900

    use List<(RawButton, KeyCode)> instead of Dictionary

--- curve/Assets/Scripts/MyController.cs
+++ curve/Assets/Scripts/MyController.cs
@@ -9,8 +9,8 @@ namespace MyUtil
 {
 
     using ButtonMapData = List<(RawButton button, KeyCode key)>;
-    using Stick2DMapData = Dictionary<Stick2D, KeyCode>;
-    using Stick3DMapData = Dictionary<Stick3D, KeyCode>;
+    using Stick2DMapData = List<(Stick2D stickDirection, KeyCode key)>;
+    using Stick3DMapData = List<(Stick3D stickDirection, KeyCode key)>;
 
     public class ButtonMap // : ButtonMapData
     {
@@ -44,8 +44,8 @@ namespace MyUtil
         });
 
         public static ButtonMap PositionalKeys = new ButtonMap(new ButtonMapData{
-            ( RawButton.A, KeyCode.Comma ),
-            ( RawButton.B, KeyCode.Period ),
+            ( RawButton.A, KeyCode.Period ),
+            ( RawButton.B, KeyCode.Slash ),
             ( RawButton.X, KeyCode.X ),
             ( RawButton.Y, KeyCode.Z ),
             ( RawButton.RIndexTrigger, KeyCode.P ),
@@ -85,58 +85,80 @@ namespace MyUtil
         Up, Down, Left, Right
     }
 
-    public class Stick2DMap : Stick2DMapData
+    public class Stick2DMap // : Stick2DMapData
     {
-        public Stick2DMap(Stick2DMapData data): base(data) { }
+        private Dictionary<Stick2D, List<KeyCode>> mappedKeys = new Dictionary<Stick2D, List<KeyCode>> { };
+        public Stick2DMap(Stick2DMapData data) { 
+            foreach (var map in data)
+            {
+                if (this.mappedKeys.ContainsKey(map.stickDirection))
+                {
+                    this.mappedKeys[map.stickDirection].Add(map.key);
+                }
+                else
+                {
+                    this.mappedKeys[map.stickDirection] = new List<KeyCode> { map.key };
+                }
+            }
+        }
 
         public static Stick2DMap Arrows = new Stick2DMap( new Stick2DMapData
         {
-            { Stick2D.Up, KeyCode.UpArrow },
-            { Stick2D.Down, KeyCode.DownArrow },
-            { Stick2D.Right, KeyCode.RightArrow },
-            { Stick2D.Left, KeyCode.LeftArrow }
+            ( Stick2D.Up, KeyCode.UpArrow ),
+            ( Stick2D.Down, KeyCode.DownArrow ),
+            ( Stick2D.Right, KeyCode.RightArrow ),
+            ( Stick2D.Left, KeyCode.LeftArrow )
         });
 
         public static Stick2DMap WASD = new Stick2DMap( new Stick2DMapData
         {
-            { Stick2D.Up, KeyCode.W },
-            { Stick2D.Down, KeyCode.S },
-            { Stick2D.Right, KeyCode.D },
-            { Stick2D.Left, KeyCode.A }
+            ( Stick2D.Up, KeyCode.W ),
+            ( Stick2D.Down, KeyCode.S ),
+            ( Stick2D.Right, KeyCode.D ),
+            ( Stick2D.Left, KeyCode.A )
         });
 
         /*
         public static Stick2DMap ijkl = new Stick2DMap(new Stick2DMapData {
-            { Stick2D.Up, KeyCode.I },
-            { Stick2D.Down, KeyCode.K },
-            { Stick2D.Right, KeyCode.L },
-            { Stick2D.Left, KeyCode.J }
+            ( Stick2D.Up, KeyCode.I ),
+            ( Stick2D.Down, KeyCode.K ),
+            ( Stick2D.Right, KeyCode.L ),
+            ( Stick2D.Left, KeyCode.J )
         });
         */
 
         public static Stick2DMap OKLSemi = new Stick2DMap(new Stick2DMapData {
-            { Stick2D.Up, KeyCode.O },
-            { Stick2D.Down, KeyCode.L },
-            { Stick2D.Right, KeyCode.K },
-            { Stick2D.Left, KeyCode.Comma }
+            ( Stick2D.Up, KeyCode.O ),
+            ( Stick2D.Down, KeyCode.L ),
+            ( Stick2D.Right, KeyCode.K ),
+            ( Stick2D.Left, KeyCode.Comma )
         });
+
+        public bool Get(Stick2D stickDirection)
+        {
+            if (!this.mappedKeys.ContainsKey(stickDirection))
+            {
+                return false;
+            }
+            return this.mappedKeys[stickDirection].Any(key => Input.GetKey(key));
+        }
 
         public Vector2 ToVector2()
         {
             Vector2 direction = new Vector2(0, 0);
-            if (Input.GetKey(this[Stick2D.Up]))
+            if (this.Get(Stick2D.Up))
             {
                 direction += new Vector2(0, 1);
             }
-            if (Input.GetKey(this[Stick2D.Down]))
+            if (this.Get(Stick2D.Down))
             {
                 direction += new Vector2(0, -1);
             }
-            if (Input.GetKey(this[Stick2D.Right]))
+            if (this.Get(Stick2D.Right))
             {
                 direction += new Vector2(1, 0);
             }
-            if (Input.GetKey(this[Stick2D.Left]))
+            if (this.Get(Stick2D.Left))
             {
                 direction += new Vector2(-1, 0);
             }
@@ -149,66 +171,89 @@ namespace MyUtil
         Up, Down, Left, Right, Above, Below
     }
 
-    public class Stick3DMap : Stick3DMapData
+    public class Stick3DMap // : Stick3DMapData
     {
-        public Stick3DMap(Stick3DMapData data): base(data) { }
+        private Dictionary<Stick3D, List<KeyCode>> mappedKeys = new Dictionary<Stick3D, List<KeyCode>> { };
+        public Stick3DMap(Stick3DMapData data)
+        {
+            foreach (var map in data)
+            {
+                if (this.mappedKeys.ContainsKey(map.stickDirection))
+                {
+                    this.mappedKeys[map.stickDirection].Add(map.key);
+                }
+                else
+                {
+                    this.mappedKeys[map.stickDirection] = new List<KeyCode> { map.key };
+                }
+            }
+        }
 
         public static Stick3DMap WASDEC = new Stick3DMap(new Stick3DMapData
         {
-            { Stick3D.Up, KeyCode.W },
-            { Stick3D.Down, KeyCode.S },
-            { Stick3D.Right, KeyCode.D },
-            { Stick3D.Left, KeyCode.A },
-            { Stick3D.Above, KeyCode.E },
-            { Stick3D.Below, KeyCode.C }
+            ( Stick3D.Up, KeyCode.W ),
+            ( Stick3D.Down, KeyCode.S ),
+            ( Stick3D.Right, KeyCode.D ),
+            ( Stick3D.Left, KeyCode.A ),
+            ( Stick3D.Above, KeyCode.E ),
+            ( Stick3D.Below, KeyCode.C )
         });
 
         /*
         public static Stick3DMap ijklum = new Stick3DMap(new Stick3DMapData
         {
-            { Stick3D.Up, KeyCode.I },
-            { Stick3D.Down, KeyCode.K },
-            { Stick3D.Right, KeyCode.L },
-            { Stick3D.Left, KeyCode.J },
-            { Stick3D.Above, KeyCode.U },
-            { Stick3D.Below, KeyCode.M }
+            ( Stick3D.Up, KeyCode.I ),
+            ( Stick3D.Down, KeyCode.K ),
+            ( Stick3D.Right, KeyCode.L ),
+            ( Stick3D.Left, KeyCode.J ),
+            ( Stick3D.Above, KeyCode.U ),
+            ( Stick3D.Below, KeyCode.M )
         });
         */
 
         public static Stick3DMap OKLSemiIComma = new Stick3DMap(new Stick3DMapData
         {
-            { Stick3D.Up, KeyCode.O },
-            { Stick3D.Down, KeyCode.L },
-            { Stick3D.Right, KeyCode.Semicolon },
-            { Stick3D.Left, KeyCode.K },
-            { Stick3D.Above, KeyCode.I },
-            { Stick3D.Below, KeyCode.Comma }
+            ( Stick3D.Up, KeyCode.O ),
+            ( Stick3D.Down, KeyCode.L ),
+            ( Stick3D.Right, KeyCode.Semicolon ),
+            ( Stick3D.Left, KeyCode.K ),
+            ( Stick3D.Above, KeyCode.I ),
+            ( Stick3D.Below, KeyCode.Comma )
         });
+
+        public bool Get(Stick3D stickDirection)
+        {
+            if (!this.mappedKeys.ContainsKey(stickDirection))
+            {
+                return false;
+            }
+            return this.mappedKeys[stickDirection].Any(key => Input.GetKey(key));
+        }
 
         public Vector3 ToVector3()
         {
             Vector3 direction = new Vector3(0, 0, 0);
-            if (Input.GetKey(this[Stick3D.Up]))
+            if (this.Get(Stick3D.Up))
             {
                 direction += new Vector3(0, 1, 0);
             }
-            if (Input.GetKey(this[Stick3D.Down]))
+            if (this.Get(Stick3D.Down))
             {
                 direction += new Vector3(0, -1, 0);
             }
-            if (Input.GetKey(this[Stick3D.Right]))
+            if (this.Get(Stick3D.Right))
             {
                 direction += new Vector3(1, 0, 0);
             }
-            if (Input.GetKey(this[Stick3D.Left]))
+            if (this.Get(Stick3D.Left))
             {
                 direction += new Vector3(-1, 0, 0);
             }
-            if (Input.GetKey(this[Stick3D.Above]))
+            if (this.Get(Stick3D.Above))
             {
                 direction += new Vector3(0, 0, 1);
             }
-            if (Input.GetKey(this[Stick3D.Below]))
+            if (this.Get(Stick3D.Below))
             {
                 direction += new Vector3(0, 0, -1);
             }
